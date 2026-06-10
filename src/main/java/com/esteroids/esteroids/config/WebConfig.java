64bea20115439eac3964cc51213af3feef319f01
Makawllim Tome <euamofurries@gmail.com>
2026-06-10 20:03:49 -0300

    Desativar WebConfig

--- src/main/java/com/esteroids/esteroids/config/WebConfig.java
+++ src/main/java/com/esteroids/esteroids/config/WebConfig.java
@@ -4,12 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+// @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Libera o Spring para ler as imagens da pasta externa em tempo real
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
-    }
+    // @Override
+    // public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    //     registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
+    // }
 }
